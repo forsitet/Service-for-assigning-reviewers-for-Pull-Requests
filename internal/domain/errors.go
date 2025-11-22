369c1e821fca2dcb8f1c085830046c58bd9a892c
Denis Budin <D20041152@yandex.ru>
2025-11-22 19:43:20 +0300

    add domain models

---
+++ internal/domain/errors.go
@@ -0,0 +1,33 @@
+package domain
+
+import "fmt"
+
+type ErrorCode string
+
+const (
+	ErrorCodeTeamExists  ErrorCode = "TEAM_EXISTS"
+	ErrorCodePRExists    ErrorCode = "PR_EXISTS"
+	ErrorCodePRMerged    ErrorCode = "PR_MERGED"
+	ErrorCodeNotAssigned ErrorCode = "NOT_ASSIGNED"
+	ErrorCodeNoCandidate ErrorCode = "NO_CANDIDATE"
+	ErrorCodeNotFound    ErrorCode = "NOT_FOUND"
+)
+
+type DomainError struct {
+	Code    ErrorCode
+	Message string
+}
+
+func (e *DomainError) Error() string {
+	if e.Message == "" {
+		return string(e.Code)
+	}
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
+func NewDomainError(code ErrorCode, msg string) *DomainError {
+	return &DomainError{
+		Code:    code,
+		Message: msg,
+	}
+}
